301b543a053ef2926cedb724423f9934c067a20e
Mohamed <merouanemohamed495@gmail.com>
2025-03-09 01:13:06 +0000

    final touch project 2 waiting for client review

--- client/app/services/page.tsx
+++ client/app/services/page.tsx
@@ -295,7 +295,7 @@ export default function Home() {
         <button
           style={{
             position: "absolute",
-            bottom: "330px",
+            bottom: "365px",
             left: "50%",
             transform: "translateX(-50%)",
             padding: "10px 20px",
